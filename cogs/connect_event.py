704d9610ec42f5496fa0d6c6c029de336da32b98
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-02 02:58:07 +0100

    Reformat the connect event cog

--- cogs/connect_event.py
+++ cogs/connect_event.py
@@ -7,80 +7,68 @@ from cogs import utils
 
 class ConnectEvent(utils.Cog):
 
+    async def send_webhook(self, text:str, username:str, logger:str) -> bool:
+        """Send a webhook to the bot specified event webhook url"""
+
+        if not self.bot.config.get("event_webhook_url"):
+            return False
+        webhook = discord.Webhook.from_url(
+            self.bot.config['event_webhook_url'],
+            adapter=discord.AsyncWebhookAdapter(self.bot.session)
+        )
+        await webhook.send(text, username=username)
+        self.logger.info(logger)
+        return True
+
     @utils.Cog.listener()
     async def on_shard_connect(self, shard_id:int):
         """Ping a given webhook when the shard ID is connected"""
 
-        if self.bot.config.get("event_webhook_url"):
-            webhook = discord.Webhook.from_url(
-                self.bot.config['event_webhook_url'],
-                adapter=discord.AsyncWebhookAdapter(self.bot.session)
-            )
-            await webhook.send(
-                f"Shard connect event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
-                username=f"{self.bot.user.name} - Shard Connect"
-            )
-            self.logger.info(f"Sent webhook for on_shard_connect event in shard `{shard_id}`")
+        await self.send_webhook(
+            f"Shard connect event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
+            f"{self.bot.user.name} - Shard Connect",
+            f"Sent webhook for on_shard_connect event in shard `{shard_id}`",
+        )
 
     @utils.Cog.listener()
     async def on_shard_ready(self, shard_id:int):
         """Ping a given webhook when the shard ID becomes ready"""
 
-        if self.bot.config.get("event_webhook_url"):
-            webhook = discord.Webhook.from_url(
-                self.bot.config['event_webhook_url'],
-                adapter=discord.AsyncWebhookAdapter(self.bot.session)
-            )
-            await webhook.send(
-                f"Shard ready event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
-                username=f"{self.bot.user.name} - Shard Ready"
-            )
-            self.logger.info(f"Sent webhook for on_shard_ready event in shard `{shard_id}`")
+        await self.send_webhook(
+            f"Shard ready event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
+            f"{self.bot.user.name} - Shard Ready",
+            f"Sent webhook for on_shard_ready event in shard `{shard_id}`",
+        )
 
     @utils.Cog.listener()
     async def on_ready(self):
         """Ping a given webhook when the bot becomes ready"""
 
-        if self.bot.config.get("event_webhook_url"):
-            webhook = discord.Webhook.from_url(
-                self.bot.config['event_webhook_url'],
-                adapter=discord.AsyncWebhookAdapter(self.bot.session)
-            )
-            await webhook.send(
-                f"Bot ready event just pinged for instance with shards `{self.bot.shard_ids}` - {dt.utcnow().strftime('%X.%f')}",
-                username=f"{self.bot.user.name} - Ready"
-            )
-            self.logger.info("Sent webhook for on_ready event")
+        await self.send_webhook(
+            f"Bot ready event just pinged for instance with shards `{self.bot.shard_ids}` - {dt.utcnow().strftime('%X.%f')}",
+            f"{self.bot.user.name} - Ready",
+            "Sent webhook for on_ready event",
+        )
 
     @utils.Cog.listener()
     async def on_shard_disconnect(self, shard_id:int):
         """Ping a given webhook when the shard ID is disconnected"""
 
-        if self.bot.config.get("event_webhook_url"):
-            webhook = discord.Webhook.from_url(
-                self.bot.config['event_webhook_url'],
-                adapter=discord.AsyncWebhookAdapter(self.bot.session)
-            )
-            await webhook.send(
-                f"Shard disconnect event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
-                username=f"{self.bot.user.name} - Shard Disconnect"
-            )
-            self.logger.info(f"Sent webhook for on_shard_disconnect event in shard `{shard_id}`")
+        await self.send_webhook(
+            f"Shard disconnect event just pinged for shard ID `{shard_id}` - {dt.utcnow().strftime('%X.%f')}",
+            f"{self.bot.user.name} - Shard Disconnect",
+            f"Sent webhook for on_shard_disconnect event in shard `{shard_id}`",
+        )
 
     @utils.Cog.listener()
     async def on_disconnect(self):
         """Ping a given webhook when the bot is disconnected"""
 
-        if self.bot.config.get("event_webhook_url"):
-            webhook = discord.Webhook.from_url(
-                self.bot.config['event_webhook_url'],
-                adapter=discord.AsyncWebhookAdapter(self.bot.session)
-            )
-            await webhook.send(
-                f"Bot disconnect event just pinged for instance with shards `{self.bot.shard_ids}` - {dt.utcnow().strftime('%X.%f')}",
-                username=f"{self.bot.user.name} - Disconnect"
-            )
-            self.logger.info("Sent webhook for on_disconnect event")
+        await self.send_webhook(
+            f"Bot disconnect event just pinged for instance with shards `{self.bot.shard_ids}` - {dt.utcnow().strftime('%X.%f')}",
+            f"{self.bot.user.name} - Disconnect",
+            "Sent webhook for on_disconnect event",
+        )
 
 
 def setup(bot:utils.Bot):
