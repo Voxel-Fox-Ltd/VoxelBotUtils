ec5e892f6ce81f017fd38d383de31ca89713ef85
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-07 23:56:16 +0100

    Change the content of the default gitignore file

--- voxelbotutils/__main__.py
+++ voxelbotutils/__main__.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         create_file("cogs", "ping_command.py", content=config.cog_example.lstrip())
         create_file("run.bat", content="py -m voxelbotutils .\n")
         create_file("run.sh", content="python3 -m voxelbotutils .\n")
-        create_file(".gitignore", content="__pycache__/\n*.toml\n!*.example*\nconfig/config.toml\n")
+        create_file(".gitignore", content="__pycache__/\nconfig/config.toml\n")
         create_file("requirements.txt", content="voxelbotutils\n")
         exit(1)
 
