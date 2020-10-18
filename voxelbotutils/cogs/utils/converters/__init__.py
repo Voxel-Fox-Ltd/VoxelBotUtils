0364dde0ae6cd16adb097b86ba5e0e0cf8119e8f
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-18 15:53:19 +0100

    Add a sexy lil custom colour converter

--- voxelbotutils/cogs/utils/converters/__init__.py
+++ voxelbotutils/cogs/utils/converters/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
 from .user_id import UserID
 from .boolean_converter import BooleanConverter
+from .colour_converter import CustomColourConverter as ColourConverter
+ColorConverter = ColourConverter  # :blobunamused: America
