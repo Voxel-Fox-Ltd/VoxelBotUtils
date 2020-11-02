ff736b579192b69507c7b6531a4c884bf94e8c99
Callum Bartlett <callum@voxelfox.co.uk>
2020-11-02 03:39:58 +0000

    Startup method is allowed to be None if there's no database

--- voxelbotutils/cogs/utils/checks/bot_is_ready.py
+++ voxelbotutils/cogs/utils/checks/bot_is_ready.py
@@ -9,7 +9,7 @@ def bot_is_ready():
     """The check for whether the bot has cached all of its data yet"""
 
     async def predicate(ctx:commands.Context):
-        if ctx.bot.is_ready() and ctx.bot.startup_method.done():
+        if ctx.bot.is_ready() and (ctx.bot.startup_method is None or ctx.bot.startup_method.done()):
             return True
         raise BotNotReady("The bot isn't marked as ready to process commands yet - please wait a minute or so.")
     return commands.check(predicate)
