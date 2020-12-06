fc4bcc871b8e11bf2bf16c76434503965429847e
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-06 04:16:34 +0000

    Fix attribute error on sending webhook

--- voxelbotutils/cogs/connect_event.py
+++ voxelbotutils/cogs/connect_event.py
@@ -16,7 +16,7 @@ class ConnectEvent(utils.Cog):
         if not event_webhook:
             return False
         try:
-            await self.bot.event_webhook.send(text, username=username)
+            await event_webhook.send(text, username=username)
         except discord.HTTPException as e:
             self.logger.error(f"Failed to send webhook for event {event_name} - {e}")
             return False
