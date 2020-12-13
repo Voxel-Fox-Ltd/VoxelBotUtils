2e6f6c2eb75363acb8e037209680ad5e92d1047f
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-13 21:52:02 +0000

    Respond with an async response and handle sending messages with a webhook class

--- voxelbotutils/cogs/slash_command_handler.py
+++ voxelbotutils/cogs/slash_command_handler.py
@@ -25,30 +25,43 @@ class InteractionMessage(object):
 
 class InteractionContext(commands.Context):
 
-    async def send(self, content, **kwargs):
-        url = "https://discord.com/api/v8/interactions/{id}/{token}/callback".format(
-            id=self._interaction_data["interaction_id"], token=self._interaction_data["token"],
-        )
-        headers = {
-            "Authorization": f"Bot {self.bot.config['token']}",
-        }
-        json = {
-            "type": 4,
-            "data": {
-                "content": content,
-            },
-        }
-        try:
-            v = await self.bot.session.post(url, json=json, headers=headers)
-        except Exception as e:
-            self.bot.logger.info(e)
-        self.total_interaction_sends += 1
-        self.bot.logger.info(await v.json())
+    # async def send(self, content, **kwargs):
+    #     # if self.total_interaction_sends == 0:
+    #     #     url = "https://discord.com/api/v8/interactions/{id}/{token}/callback".format(
+    #     #         id=self._interaction_data["interaction_id"], token=self._interaction_data["token"],
+    #     #     )
+    #     # else:
+    #     url = "https://discord.com/api/v8/webhooks/{id}/{token}".format(
+    #         id=self._interaction_data["interaction_id"], token=self._interaction_data["token"],
+    #     )
+    #     headers = {
+    #         "Authorization": f"Bot {self.bot.config['token']}",
+    #     }
+    #     # json = {
+    #     #     "type": 4,
+    #     #     "data": {
+    #     #         "content": content,
+    #     #     },
+    #     # }
+    #     json = {
+    #         "content": content,
+    #     }
+    #     # json.update(kwargs)
+    #     try:
+    #         v = await self.bot.session.post(url, json=json, headers=headers)
+    #     except Exception as e:
+    #         self.bot.logger.info(e)
+    #     self.total_interaction_sends += 1
+    #     self.bot.logger.info(await v.json())
+
+    async def send(self, *args, **kwargs):
+        return await self._interaction_webhook.send(*args, **kwargs, wait=True)
 
 
 class SlashCommandHandler(utils.Cog):
 
     async def get_context_from_interaction(self, payload, *, cls=InteractionContext):
+        # Make a context
         view = commands.view.StringView(f"<@{self.bot.user.id}> {payload['data']['name']} {' '.join([i['value'] for i in payload['data']['options']])}")
         fake_message = InteractionMessage(
             guild=self.bot.get_guild(int(payload['guild_id'])),
@@ -61,11 +74,28 @@ class SlashCommandHandler(utils.Cog):
         ctx = cls(prefix=None, view=view, bot=self.bot, message=fake_message)
         view.skip_string(f"<@{self.bot.user.id}> ")
         invoker = view.get_word()
+
+        # Make it work
         ctx.invoked_with = invoker
         ctx.prefix = f"<@{self.bot.user.id}> "
-        ctx._interaction_data = {"token": payload["token"], "interaction_id": payload["id"], "command_id": payload["data"]["id"]}
+        # ctx._interaction_data = {"token": payload["token"], "interaction_id": payload["id"], "command_id": payload["data"]["id"]}
+        ctx._interaction_webhook = discord.Webhook.partial(payload["token"], payload["id"], adapter=discord.AsyncWebhookAdapter(self.bot.session))
         ctx.command = self.bot.all_commands.get(invoker)
-        ctx.total_interaction_sends = 0
+        # ctx.total_interaction_sends = 0
+
+        # Send async data response
+        url = "https://discord.com/api/v8/interactions/{id}/{token}/callback".format(
+            id=ctx._interaction_data["interaction_id"], token=ctx._interaction_data["token"],
+        )
+        json = {
+            "type": 5,
+        }
+        headers = {
+            "Authorization": f"Bot {self.bot.config['token']}",
+        }
+        await self.bot.session.post(url, json=json, headers=headers)
+
+        # Return context
         return ctx
 
     @utils.Cog.listener()
