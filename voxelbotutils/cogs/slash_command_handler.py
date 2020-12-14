0bbb7e08887eba3162fd979e237df7d0b1ff23b1
Callum Bartlett <callum@voxelfox.co.uk>
2020-12-14 20:01:55 +0000

    Add application command dataclasses

--- voxelbotutils/cogs/slash_command_handler.py
+++ voxelbotutils/cogs/slash_command_handler.py
@@ -1,7 +1,102 @@
+import typing
+import enum
+
 import discord
 from discord.ext import commands
 
 from . import utils
+
+
+class ApplicationCommandOptionType(enum.IntEnum):
+    SUBCOMMAND = 1
+    SUBCOMMAND_GROUP = 2
+    STRING = 3
+    INTEGER = 4
+    BOOLEAN = 5
+    USER = 6
+    CHANNEL = 7
+    ROLE = 8
+
+
+class ApplicationCommandOptionChoice(object):
+
+    def __init__(self, name:str, value:typing.Any):
+        self.name: str = name
+        self.value: typing.Any = value
+
+    @classmethod
+    def from_data(cls, data:dict):
+        return cls(data['name'], data['value'])
+
+    def to_json(self) -> dict:
+        return {"name": self.name, "value": self.value}
+
+
+class ApplicationCommandOption(object):
+
+    def __init__(self, name:str, type:ApplicationCommandOptionType, description:str, default:typing.Optional[str]=None, required:bool=True):
+        self.name: str = name
+        self.type: ApplicationCommandOptionType = type
+        self.description: str = description
+        self.default: typing.Any = default
+        self.required: bool = required
+        self.choices: typing.List[ApplicationCommandOptionChoice] = list()
+        self.options: typing.List['ApplicationCommandOption'] = list()
+
+    def add_choice(self, choice:ApplicationCommandOptionChoice) -> None:
+        self.choices.append(choice)
+
+    def add_option(self, option:'ApplicationCommandOption') -> None:
+        self.options.append(option)
+
+    @classmethod
+    def from_data(cls, data:dict):
+        base_option = cls(data['name'], ApplicationCommandOptionType(data['type']), data['description'], data.get('required', False))
+        for choice in data['choices']:
+            base_option.add_choice(ApplicationCommandOptionChoice.from_data(choice))
+        for option in data['options']:
+            base_option.add_option(cls.from_data(option))
+        return base_option
+
+    def to_json(self) -> dict:
+        return {
+            "name": self.name,
+            "type": self.type.value,
+            "description": self.description,
+            "default": self.default,
+            "required": self.required,
+            "choices": [i.to_json() for i in self.choices],
+            "options": [i.to_json() for i in self.options],
+        }
+
+
+class ApplicationCommand(object):
+
+    def __init__(self, name:str, description:str):
+        self.name: str = name
+        self.description: str = description
+        self.options: typing.List[ApplicationCommandOption] = list()
+        self.id: int = None
+        self.application_id: int = None
+
+    def add_option(self, option:ApplicationCommandOption):
+        self.options.append(option)
+
+    @classmethod
+    def from_data(cls, data:dict):
+        command = cls(data['name'], data['description'])
+        command.id = int(data['id'])
+        command.application_id = int(data['application_id'])
+        for option in data['options']:
+            command.add_option(ApplicationCommandOption.from_data(option))
+        return command
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "options": [i.to_json() for i in self.options],
+        }
 
 
 class InteractionMessage(object):
@@ -28,20 +123,21 @@ class InteractionContext(commands.Context):
         return await self._interaction_webhook.send(*args, wait=True, **kwargs)
 
 
-class V8AsyncWebhookAdapter(discord.AsyncWebhookAdapter):
-    BASE = "https://discord.com/api/v8"
-
-
 class SlashCommandHandler(utils.Cog):
 
     COMMAND_TYPE_MAPPER = {
-        discord.User: 6,
-        discord.Member: 6,
-        discord.TextChannel: 7,
-        discord.Role: 8,
-        str: 3,
-        int: 4,
+        discord.User: ApplicationCommandOptionType.USER,
+        discord.Member: ApplicationCommandOptionType.USER,
+        discord.TextChannel: ApplicationCommandOptionType.CHANNEL,
+        discord.Role: ApplicationCommandOptionType.ROLE,
+        str: ApplicationCommandOptionType.STRING,
+        int: ApplicationCommandOptionType.INTEGER,
     }
+
+    def __init__(self, bot:utils.Bot):
+        super().__init__(bot)
+        self.commands: typing.List[ApplicationCommand] = None
+        self.application_id = None
 
     async def get_context_from_interaction(self, payload, *, cls=InteractionContext):
         # Make a context
@@ -54,24 +150,21 @@ class SlashCommandHandler(utils.Cog):
             data=payload,
             content=view.buffer,
         )
-        ctx = cls(prefix=None, view=view, bot=self.bot, message=fake_message)
+        ctx = cls(prefix=f"<@{self.bot.user.id}> ", view=view, bot=self.bot, message=fake_message)
         view.skip_string(f"<@{self.bot.user.id}> ")
         invoker = view.get_word()
 
         # Make it work
         ctx.invoked_with = invoker
-        ctx.prefix = f"<@{self.bot.user.id}> "
-        ctx._interaction_webhook = discord.Webhook.partial(ctx.bot.user.id, payload["token"], adapter=V8AsyncWebhookAdapter(self.bot.session))
-        self.logger.info(f"Set response url to {ctx._interaction_webhook._adapter._request_url}")
+        ctx._interaction_webhook = discord.Webhook.partial(
+            await self.get_application_id(), payload["token"],
+            adapter=discord.AsyncWebhookAdapter(self.bot.session)
+        )
         ctx.command = self.bot.all_commands.get(invoker)
 
         # Send async data response
         url = "https://discord.com/api/v8/interactions/{id}/{token}/callback".format(id=payload["id"], token=payload["token"])
-        json = {"type": 5}
-        headers = {"Authorization": f"Bot {self.bot.config['token']}"}
-        self.logger.info("Sending type 5 statement")
-        await self.bot.session.post(url, json=json, headers=headers)
-        self.logger.info("Sent - returning context")
+        await self.bot.session.post(url, json={"type": 5}, headers={"Authorization": f"Bot {self.bot.config['token']}"})
 
         # Return context
         return ctx
@@ -82,9 +175,49 @@ class SlashCommandHandler(utils.Cog):
             return
         self.logger.info("Received interaction payload %s" % (str(payload)))
         ctx = await self.get_context_from_interaction(payload['d'])
-        self.logger.info("Received context object")
         await self.bot.invoke(ctx)
-        self.logger.info("Invoked context object")
+
+    @staticmethod
+    def is_typing_optional(annotation):
+        """
+        Stolen from Rapptz - https://github.com/Rapptz/discord.py/blob/60f804c63298d5f46a5ae4352b049d91b16d1b8c/discord/ext/commands/core.py#L975-L984
+        """
+
+        if annotation.default is None:
+            return True
+        try:
+            origin = annotation.__origin__
+        except AttributeError:
+            return False
+        if origin is not typing.Union:
+            return False
+        if len(annotation.__args__) != 2:
+            return False
+        return annotation.__args__[-1] is type(None)
+
+    @staticmethod
+    def get_non_optional_type(annotation):
+        return annotation.__args__[0]
+
+    async def get_application_id(self):
+        if self.application_id:
+            return self.application_id
+        app = await self.bot.application_info()
+        self.application_id = app.id
+
+    async def get_slash_commands(self):
+        """
+        Get the application's global command objects.
+        """
+
+        if self.commands is not None:
+            return self.commands
+        url = "https://discord.com/api/applications/{application_id}/commands".format(application_id=await self.get_application_id())
+        headers = {"Authorization": f"Bot {self.bot.config['token']}"}
+        site = await self.bot.session.get(url, headers=headers)
+        data = await site.json()
+        self.commands = [ApplicationCommand.from_data(i) for i in data]
+        return self.commands
 
     @commands.command(cls=utils.Command)
     @commands.is_owner()
@@ -98,9 +231,12 @@ class SlashCommandHandler(utils.Cog):
         filtered_commands = await self.bot.help_command.filter_commands_classmethod(ctx, commands)
         for command in filtered_commands:
             for arg in command.clean_params.values():
-                if arg.annotation not in self.COMMAND_TYPE_MAPPER:
-                    commands_we_cant_deal_with.append(command.name + " " + str(command.clean_params.values()))
-                    break
+                if arg.annotation in self.COMMAND_TYPE_MAPPER:
+                    continue
+                if self.is_typing_optional(arg.annotation) and self.get_non_optional_type(arg.annotation) in self.COMMAND_TYPE_MAPPER:
+                    continue
+                commands_we_cant_deal_with.append(command.name + " " + str(command.clean_params.values()))
+                break
         if commands_we_cant_deal_with:
             return await ctx.send("\n".join(commands_we_cant_deal_with))
         return await ctx.send("I can deal with all of these.")
