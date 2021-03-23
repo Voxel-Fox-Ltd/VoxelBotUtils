87f9f4c09a846f5f4ee21520a055dcb63656fabe
Kae Bartlett <Kae@voxelfox.co.uk>
2021-03-23 20:16:20 +0000

    Add missing import

--- voxelbotutils/cogs/utils/checks/cooldown/__init__.py
+++ voxelbotutils/cogs/utils/checks/cooldown/__init__.py
@@ -1,3 +1,3 @@
-from .cooldown import Cooldown, CooldownMapping, cooldown, GroupedCooldownMapping, NoRaiseCommandOnCooldown, NoRaiseCooldown
+from .cooldown import Cooldown, CooldownMapping, cooldown, GroupedCooldownMapping, NoRaiseCommandOnCooldown, NoRaiseCooldown, no_raise_cooldown
 from .cooldown_with_exemptions import CooldownWithChannelExemptions
 from .role_based_cooldown import RoleBasedCooldown
