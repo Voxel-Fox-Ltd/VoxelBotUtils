f47e5264ab682cb62c40a9568c256147067e3aff
George M <32426140+iHeroGH@users.noreply.github.com>
2021-04-28 22:31:47 -0700

    Made ActionRow subscriptable
    Closes #70

--- voxelbotutils/cogs/utils/interactions/components/__init__.py
+++ voxelbotutils/cogs/utils/interactions/components/__init__.py
@@ -11,3 +11,11 @@ class ActionRow(object):
             "type": 1,
             "components": [i.to_dict() for i in self.components],
         }
+    
+    def __getitem__(self, custom_id: str):
+        for i in self.components:
+            if getattr(i, "custom_id", None) == custom_id:
+                return i
+        
+        raise KeyError(f"There is no component with the custom ID {custom_id}")
+        
