f87a3fb005874bde054af918304b9f1223b13cda
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-30 03:10:09 +0100

    Apparently the original callback doesn't send you back a message response

--- voxelbotutils/cogs/utils/custom_bot.py
+++ voxelbotutils/cogs/utils/custom_bot.py
@@ -850,7 +850,8 @@ class Bot(commands.AutoShardedBot):
         if isinstance(channel, (list, tuple)):
             if not messagable._sent_ack_response:  # Sent no responses
                 r = discord.http.Route('POST', '/interactions/{interaction_id}/{token}/callback', interaction_id=channel[0], token=channel[2])
-            elif not messagable._sent_message_response:  # Sent an ack but not a message
+                await self.http.request(r, json={"type": messageable.ACK_RESPONSE_TYPE})
+            if not messagable._sent_message_response:  # Sent an ack but not a message
                 r = discord.http.Route('PATCH', '/webhooks/{app_id}/{token}/messages/@original', app_id=channel[1], token=channel[2])
             else:  # Sent both an ack and a message
                 r = discord.http.Route('POST', '/webhooks/{app_id}/{token}', app_id=channel[1], token=channel[2])
