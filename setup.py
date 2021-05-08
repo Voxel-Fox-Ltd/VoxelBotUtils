c90f85e86842cb1a942b6d0c964735ad6e504d60
Kae Bartlett <kae@voxelfox.co.uk>
2021-05-08 22:15:13 +0100

    Fix license in the setup file

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
