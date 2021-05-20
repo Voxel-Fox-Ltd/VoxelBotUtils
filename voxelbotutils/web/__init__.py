964816ab37b30c1eda5e24104b7e260a35467e42
Kae Bartlett <Kae@voxelfox.co.uk>
2021-05-20 06:29:38 +0100

    Fix missing import

--- voxelbotutils/web/__init__.py
+++ voxelbotutils/web/__init__.py
@@ -5,3 +5,4 @@ from .utils.process_discord_login import (
     get_discord_login_url, process_discord_login, get_user_info_from_session, get_access_token_from_session,
     get_user_guilds_from_session, add_user_to_guild_from_session,
 )
+from .utils.web_context import WebContext
