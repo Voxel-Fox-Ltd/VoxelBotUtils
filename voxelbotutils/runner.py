0731d4bf56f8bac091c66dfa0e5d4bd98bf0e248
Kae Bartlett <kae@voxelfox.co.uk>
2021-05-22 02:00:44 +0100

    Render our emojis as images instead of styled spans

--- voxelbotutils/runner.py
+++ voxelbotutils/runner.py
@@ -436,7 +436,7 @@ def run_website(args: argparse.Namespace) -> None:
     def display_emojis(string):
         return re.sub(
             r"(?P<emoji>(?:<|&lt;)(?P<animated>a)?:(?P<name>\w+):(?P<id>\d+)(?:>|&gt;))",
-            r'<span class="discord_emoji" data-id="\g<id>" data-animated="\g<animated>" data-url="https://cdn.discordapp.com/emojis/\g<id>.png"></span>',
+            lambda g: f'<img class="discord_emoji" src="https://cdn.discordapp.com/emojis/{g.group("id")}.{"gif" if g.group("animated") else "png"}" style="height: 1rem; width: auto;">'
             string,
             re.IGNORECASE | re.MULTILINE,
         )
