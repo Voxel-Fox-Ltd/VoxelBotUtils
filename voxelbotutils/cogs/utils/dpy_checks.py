52c60dc4875fe728c6f4f21dd3ff1caaf203b0a4
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-05 17:08:16 +0100

    Fix typo

--- voxelbotutils/cogs/utils/dpy_checks.py
+++ voxelbotutils/cogs/utils/dpy_checks.py
@@ -5,7 +5,7 @@ from .interactions.interaction_messageable import InteractionMessageable
 
 def bot_has_permissions(*args, **kwargs):
     async def predicate(ctx):
-        if isinstasnce(ctx, InteractionMessageable):
+        if isinstance(ctx, InteractionMessageable):
             return True
         return await commands.bot_has_permissions(*args, **kwargs).predicate(ctx)
     return commands.check(predicate)
@@ -13,7 +13,7 @@ def bot_has_permissions(*args, **kwargs):
 
 def bot_has_guild_permissions(*args, **kwargs):
     async def predicate(ctx):
-        if isinstasnce(ctx, InteractionMessageable):
+        if isinstance(ctx, InteractionMessageable):
             return True
         return await commands.bot_has_guild_permissions(*args, **kwargs).predicate(ctx)
     return commands.check(predicate)
