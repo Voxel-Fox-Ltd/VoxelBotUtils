1c7161a26063e502092ff8ec621bb0d4330a286b
Kae Bartlett <kae@voxelfox.co.uk>
2021-12-23 23:41:19 +0000

    Change avatar and icon for oauth models

--- voxelbotutils/web/utils/oauth_models.py
+++ voxelbotutils/web/utils/oauth_models.py
@@ -20,20 +20,17 @@ class OauthGuild(object):
     def __init__(self, bot, guild_data, user):
         self.id: int = int(guild_data.get("id"))
         self.name: str = guild_data.get("name")
-        self.icon: str = guild_data.get("icon")
+        self._icon: typing.Optional[str] = guild_data.get("icon")
         self.owner_id: int = user.id if guild_data.get("owner") else 0
         self.features: typing.List[str] = guild_data.get("features")
         self._bot: discord.Client = bot
 
-    def is_icon_animated(self) -> bool:
-        return self.icon.startswith("a_")
-
     @property
-    def icon_url(self):
-        return self.icon_url_as()
-
-    def icon_url_as(self, *, format=None, static_format='webp', size=1024):
-        return discord.Asset._from_guild_icon(None, self, format=format, static_format=static_format, size=size)
+    def icon(self) -> typing.Optional[discord.Asset]:
+        """Optional[:class:`Asset`]: Returns the guild's icon asset, if available."""
+        if self._icon is None:
+            return None
+        return discord.Asset._from_guild_icon(None, self.id, self._icon)
 
     async def fetch_guild(self, bot=None) -> typing.Optional[discord.Guild]:
         """
@@ -72,21 +69,18 @@ class OauthUser(object):
     def __init__(self, user_data):
         self.id: int = int(user_data['id'])
         self.username: str = user_data.get("username")
-        self.avatar: str = user_data.get("avatar")
+        self._avatar: str = user_data.get("avatar")
         self.discriminator: str = user_data.get("discriminator")
         self.public_flags: discord.PublicUserFlags = discord.PublicUserFlags._from_value(user_data.get("public_flags", 0))
         self.locale: str = user_data.get("locale")
         self.mfa_enabled: bool = user_data.get("mfa_enabled", False)
 
-    def is_avatar_animated(self) -> bool:
-        return self.avatar.startswith("a_")
-
     @property
-    def avatar_url(self):
-        return self.avatar_url_as()
-
-    def avatar_url_as(self, *, format=None, static_format='webp', size=1024):
-        return discord.Asset._from_avatar(None, self, format=format, static_format=static_format, size=size)
+    def avatar(self) -> typing.Optional[discord.Asset]:
+        """Optional[:class:`Asset`]: Returns the guild's icon asset, if available."""
+        if self._avatar is None:
+            return None
+        return discord.Asset._from_avatar(None, self.id, self._avatar)
 
 
 class OauthMember(OauthUser):
