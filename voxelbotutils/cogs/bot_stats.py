4cf1f7c89d65a49b56a9d1702ae95fcea9e6e0c9
Kae Bartlett <kae@voxelfox.co.uk>
2022-01-20 06:43:45 +0000

    Add a defer to info now that it performs web requests

--- voxelbotutils/cogs/bot_stats.py
+++ voxelbotutils/cogs/bot_stats.py
@@ -13,6 +13,7 @@ class BotStats(vbu.Cog):
     @vbu.command(
         application_command_meta=commands.ApplicationCommandMeta(),
     )
+    @commands.defer()
     @vbu.checks.is_config_set('bot_info', 'enabled')
     @commands.bot_has_permissions(send_messages=True, embed_links=True)
     async def info(self, ctx: vbu.Context):
