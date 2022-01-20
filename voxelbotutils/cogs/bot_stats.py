a98d46d16174fbc48f257591aac858bc60b25746
Kae Bartlett <kae@voxelfox.co.uk>
2022-01-20 07:07:47 +0000

    Only add shard count if not a webserver

--- voxelbotutils/cogs/bot_stats.py
+++ voxelbotutils/cogs/bot_stats.py
@@ -143,8 +143,8 @@ class BotStats(vbu.Cog):
                 )
             else:
                 embed.add_field("Guild Count", f"{len(self.bot.guilds):,}")
-        embed.add_field("Shard Count", f"{self.bot.shard_count or 1:,}")
         if self.bot.latency >= 0:
+            embed.add_field("Shard Count", f"{self.bot.shard_count or 1:,}")
             embed.add_field("Average WS Latency", f"{(self.bot.latency * 1000):.2f}ms")
 
         # Get topgg data
